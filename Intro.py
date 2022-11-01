dc6e32047b18f7969d36e9bb46c54fce9b15dff4
Lorenzo900 <ricci.lorenzo90@gmail.com>
2022-11-01 15:52:24 +0100

    ok

--- Intro.py
+++ Intro.py
@@ -25,6 +25,7 @@ MENU = {
 }
 
 profit = 0
+machine_active = True
 resources = {
     "water": 300,
     "milk": 200,
@@ -35,20 +36,24 @@ resources = {
 
 user_input = input("What would you like? (espresso/latte/cappuccino):\n").lower()
 
-machine_active = True
 
-# TODO: 2. create a function that do the report when asked to
+# TODO: 2. Turn off the machine by entering off to the prompt
+
+if user_input == "off":
+    machine_active = False
+
+
+# TODO: 3. create a function that do the report when asked to
 
 
 def print_report():
     print(f'Water: {resources["water"]} ml\n Milk: {resources["milk"]} ml\n Coffee: {resources["coffee"]} ml')
 
 
-print_report()
+if user_input == "report":
+    print_report()
+
+# TODO: 3. Check if resources are enough
 
 
-
-
-
-
-
+def check_resources():
